8625d1ca5b0bdfbfe1a86c64f662324260f13874
huluo666 <huluo@huluodeMacBook-Pro.local>
2015-11-24 21:24:06 +0800

    还没写

---
+++ HJAutoFMDB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HJAutoFMDB
+//
+//  Created by luo.h on 15/11/24.
+//  Copyright © 2015年 huluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HJAutoFMDB/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HJAutoFMDB
+//
+//  Created by luo.h on 15/11/24.
+//  Copyright © 2015年 huluo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
